12f8a3fa28ab443a2f6133cde1ea4b7e7c72a58c
Ahmed-Sherif-ASA <ahmedsherifakl.asa@gmail.com>
2020-05-18 02:06:02 +0200

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -33,7 +33,7 @@ class Dishdetail extends Component {
             <div key={comm.id} className="col-12 col-md m-1">
               <div tag="li">
                 <p>{comm.comment}</p>
-                <p>-- {comm.author}, {comm.date}</p> 
+                <p>-- {comm.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comm.date)))}</p> 
               </div>
             </div>
           </div>
@@ -55,12 +55,14 @@ class Dishdetail extends Component {
 
   render() {
     return(
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          {this.renderDish(this.props.selectedDish)}
-        </div>
-        <div className="col-12 col-md-5 m-1"> 
-            {this.renderComments(this.props.selectedDish)}
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.props.dish)}
+          </div>
+          <div className="col-12 col-md-5 m-1"> 
+              {this.renderComments(this.props.dish)}
+          </div>
         </div>
       </div>
     );
