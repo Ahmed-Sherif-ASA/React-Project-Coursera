92cb94ac7c21db12770f07fbe2520a7d082430ed
Ahmed-Sherif-ASA <ahmedsherifakl.asa@gmail.com>
2020-05-26 23:38:41 +0200

    Single page applications part two

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -22,7 +23,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
   function RenderComments({ dishComment }) {
     if (dishComment != null){
-      const cmnt = dishComment.comments.map((comm) => {
+      console.log(dishComment);
+      const cmnt = dishComment.map((comm) => {
         return (
           <div className="row">
             <div key={comm.id} className="col-12 col-md m-1">
@@ -41,10 +43,11 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         </div>
     );   
       } else {
+        console.log("dish comments empty");
         return(
-          <div></div>
+          <div> </div>
       );
-      }
+    }
 
   }
 
@@ -52,11 +55,21 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     return(
       <div className="container">
         <div className="row">
+            <Breadcrumb>
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
           <div className="col-12 col-md-5 m-1">
             <RenderDish dish={props.dish}/>
           </div>
           <div className="col-12 col-md-5 m-1"> 
-            <RenderComments dishComment={props.dish}/>
+            <RenderComments dishComment={props.comments}/>
           </div>
         </div>
       </div>
